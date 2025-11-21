bc077caa06e245ec5348bf8202190a8935daf136
Martin Egholm Sørensen <mas025@edu.zealand.dk>
2025-11-21 13:25:40 +0100

    som folders

---
+++ gategourmetLibary/Repo/CustomerRepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gategourmetLibrary.Repo
+{
+    internal class CustomerRepo
+    {
+
+
+        public void Add(int customer)
+        {
+
+        }
+
+        public int Get()
+        {
+            int i = 1;
+            return i;
+        }
+    }
+}
