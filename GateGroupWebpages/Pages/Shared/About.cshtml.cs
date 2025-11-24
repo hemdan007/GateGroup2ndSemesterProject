8ec0a9bdf7caf9b126de47196ee94860122ca72e
HelinSaraBaran <sara501d@edu.zealand.dk>
2025-11-24 13:05:01 +0100

    more changes

---
+++ GateGroupWebpages/Pages/Shared/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GateGroupWebpages.Pages.Shared
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
