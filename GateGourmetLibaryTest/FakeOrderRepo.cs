d8a36ef2432e6b4d859a66a5d7c068a466b3dd1f
crestfall <mas025@edu.zealand.dk>
2025-12-17 01:06:43 +0100

    added 1 test that works

---
+++ GateGourmetLibaryTest/FakeOrderRepo.cs
@@ -0,0 +1,111 @@
+﻿using gategourmetLibrary.Models;
+using gategourmetLibrary.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GateGourmetLibaryTest
+{
+    internal class FakeOrderRepo : IOrderRepo
+    {
+
+        public Dictionary<int,Order> DictOrders = new Dictionary<int, Order>();
+
+        public void AddOrder(Order order)
+        {
+            DictOrders.Add(order.ID,order);
+        }
+
+        public void CancelOrder(int orderId)
+        {
+            DictOrders[orderId].Status = OrderStatus.Cancelled;
+        }
+
+        public void DeleteOrder(int ID)
+        {
+            DictOrders.Remove(ID);
+        }
+
+        public List<Order> FilterByCompany(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> FilterByDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> FilterByDepartment(int departmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> FilterByEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> FilterByStatus(OrderStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> FilterByToday(DateTime today)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order Get(int orderID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, string> GetAllAllergies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, Ingredient> GetAllIngredients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Warehouse> GetAllWarehouses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Warehouse GetRecipePartLocation(int recipePartId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RecipePart> GetRecipeParts(int orderID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrder(int orderID, Order updatedOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRecipePartLocation(int recipePartId, int warehouseId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GateGourmetLibaryTest/OrderTest.cs
@@ -0,0 +1,37 @@
+﻿using gategourmetLibrary.Models;
+using gategourmetLibrary.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Diagnostics;
+
+namespace GateGourmetLibaryTest
+{
+    public class OrderTest
+    {
+        [Theory]
+        [InlineData(1)]
+        public void AddTest(int id)
+        {
+            FakeOrderRepo orderRepo = new FakeOrderRepo();
+            OrderService orderService = new OrderService(orderRepo);
+            Order order = new Order();
+            order.ID = id;
+            order.OrderMade = DateTime.Now;
+            order.OrderDoneBy = DateTime.Now.AddDays(7);
+            order.Recipe.Add(1,new RecipePart());
+            order.Recipe[1].partName = "test";
+
+
+            orderService.AddOrder(order);
+
+
+            Assert.Equal(id, orderRepo.DictOrders[id].ID);
+        }
+
+
+    }
+}
